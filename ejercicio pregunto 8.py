af0dfb6898e77be9fe0f748963666fd00e5c23da
Fernandoramos24 <87905334+Fernandoramos24@users.noreply.github.com>
2021-09-11 10:40:18 -0600

    ejercicio pregunta 8

---
+++ ejercicio pregunto 8.py
@@ -0,0 +1,11 @@
+#Cómo se puede generar múltiples variables en Python y asignarles un valor a su vez
+
+x, y, z = "marcela", "william", "fernando"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "marcela"
+print(x)
+print(y)
+print(z)
